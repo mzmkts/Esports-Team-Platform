5af9af9dc4df7a5c2a39b942c0244649d90d42c0
mzmkts <glemillov@gmail.com>
2026-05-18 23:40:59 +0500

    fix: update CORS origins to allow Vercel deployment

--- backend/app.js
+++ backend/app.js
@@ -10,18 +10,7 @@ const { createRouteHandler } = require("uploadthing/express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, Accept, X-Requested-With");
-    res.setHeader("Vary", "Origin");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-    next();
-});
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
